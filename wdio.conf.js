2b97e0521eda0340a24d72b261d0c496b1ba5a99
vvoldi <98.kovalenko@gmail.com>
2022-10-27 15:26:59 +0300

    add tests

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,7 @@ exports.config = {
     logLevel: "debug",
     bail: 0,
     baseUrl: "https://telnyx.com/",
-    waitforTimeout: 40000,
+    waitforTimeout: 60000,
     connectionRetryTimeout: 120000,
     services: ["chromedriver"],
     framework: "mocha",
